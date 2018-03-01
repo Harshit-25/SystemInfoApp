763fc245db03aed6906043c742abeb8ed07713bb
Harshit Gupta <harshitguptaggc@gmail.com>
2018-03-01 09:52:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const {app, BrowserWindow} = require('electron')
+  const path = require('path')
+  const url = require('url')
+  //Init win
+  let win;
+
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+
+      win = null
+    });
+  }
+
+//Run create window function
+  app.on('ready', createWindow)
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  });
+
+  app.on('activate', () => {
+
+    if (win === null) {
+      createWindow()
+    }
+  });
---
+++ renderer.js
@@ -0,0 +1,1 @@
+//This file is require to load the index.html file.
